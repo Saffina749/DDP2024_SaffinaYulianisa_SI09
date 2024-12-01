4ff3a78554ec85fb967946f75fd6570a56826a34
Saffina749 <saffinayulianisa15@gmail.com>
2024-12-01 11:29:20 +0700

    Add files via upload

---
+++ call.py
@@ -0,0 +1,14 @@
+import hitung
+
+hitung.tambah(5, 5)
+hitung.kali(4, 2)
+hitung.kurang(10, 3)
+hitung.pangkat(2, 4)
+
+import bangundatar
+
+bangundatar.l_persegi(5)
+bangundatar.l_persegi_panjang(2, 5)
+bangundatar.l_segitiga(2, 5)
+bangundatar.l_lingkaran(1)
+bangundatar.l_jajargenjang(2, 5)
